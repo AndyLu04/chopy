ca537cc792c396436630d1b1af7f1768743a232b
AndyLu04 <36753978+AndyLu04@users.noreply.github.com>
2020-06-10 23:53:50 +0800

    Update y.cpp

--- y.cpp
+++ y.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <fstream.h>
 
 using namespace std;
 
 int main()
 {
 	cout << "HW" << endl;
+	fstream file;
+	file.open("Reader.txt", ios::out | ios::trunc);
+	file.write("abc\n", sizeof("abc\n"));
+	file.close(); 
+		
+	char str[100];		
+
+	file.open("Reader.txt", ios::in);
+	file.read(str,sizeof("abc\n"));
+	cout << str << endl;
+	
 	return 0;
 }
